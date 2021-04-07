b962eada33beedff7b84ef6f8d71df49e9795486
Diana Leon <dianalucialf@ufps.edu.co>
2021-04-06 21:35:10 -0500

    arqui

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Restaurante</title>
+	<link rel="stylesheet" href="./style.css" type="text/css">
+
+</head>
+<body>
+	<section class="fondo">
+
+		<div  class="e1">
+			<h1 style="text-align:center;">Restaurante</h1>
+		</div>
+
+		<div class="e2" style="text-align:center;">
+		</br></br>
+			<a href="staff" >Empleado</a>
+			<a href="admin">Administrador</a>
+		</div>
+
+  		<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+	
+	</section>
+</body>
+</html>
